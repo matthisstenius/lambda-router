a5ba7ca1166d1749dd3ceec955c823daae411266
Matthis Stenius <matthis@me.com>
2017-11-30 21:10:25 +0100

    initializer for response

--- response.go
+++ response.go
@@ -1,6 +1,15 @@
 package api
 
+import (
+	"encoding/json"
+)
+
 type Response struct {
 	StatusCode int
 	Body interface{}
+}
+
+func NewResponse(status int, body interface{}) Response {
+	encoded, _ := json.Marshal(body)
+	return Response{StatusCode: status, Body: string(encoded)}
 }
