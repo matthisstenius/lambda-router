4a9135ece9c9e9b0c3090437979e67efaa340c44
Matthis Stenius <matthis@me.com>
2017-12-03 16:22:43 +0100

    can now route resources with path params

--- request.go
+++ request.go
@@ -2,26 +2,35 @@ package api
 
 import (
 	"errors"
+	"strings"
+	"fmt"
 )
 
 type Request struct {
 	resource string
-	method string
-	event map[string]interface{}
-	routes map[string]map[string]func(i Input)Response
+	method   string
+	event    map[string]interface{}
+	routes   map[string]map[string]func(i Input) Response
 }
 
-func NewRequest(event interface{}, routes map[string]map[string]func(i Input)Response) Request {
+func NewRequest(event interface{}, routes map[string]map[string]func(i Input) Response) Request {
 	return Request{
 		resource: event.(map[string]interface{})["resource"].(string),
-		method: event.(map[string]interface{})["httpMethod"].(string),
-		event: event.(map[string]interface{}),
-		routes: routes,
+		method:   event.(map[string]interface{})["httpMethod"].(string),
+		event:    event.(map[string]interface{}),
+		routes:   routes,
 	}
 }
 
 func (r Request) Invoke() (Response, error) {
-	handler, ok := r.routes[r.resource][r.method]
+	resource := r.resource
+	pathParams := r.event["pathParameters"].(map[string]interface{})
+	
+	for k, v := range pathParams {
+		resource = strings.Replace(resource, v.(string), fmt.Sprintf("{%s}", k), 1)
+	}
+
+	handler, ok := r.routes[resource][r.method]
 
 	var response Response
 	if !ok {
