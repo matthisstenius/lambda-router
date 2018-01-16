102b9bec60c9649b950097dced256f534167215a
Matthis Stenius <matthis@auddly.com>
2018-01-16 16:38:22 +0100

    Beginning of SNS event

--- request.go
+++ request.go
@@ -12,35 +12,48 @@ type Request struct {
     method   string
     event    map[string]interface{}
     routes   map[string]map[string]func(i *Input) *Response
+    events map[string]func(i *Input) *Response
 }
 
-func NewRequest(event interface{}, routes map[string]map[string]func(i *Input) *Response) *Request {
+func NewRequest(event interface{}, routes map[string]map[string]func(i *Input) *Response, events map[string]func(i *Input) *Response) *Request {
     return &Request{
         resource: event.(map[string]interface{})["resource"].(string),
         method:   event.(map[string]interface{})["httpMethod"].(string),
         event:    event.(map[string]interface{}),
         routes:   routes,
+        events: events,
     }
 }
 
 func (r *Request) Invoke() (*Response, error) {
     log.Printf("Request event: %s", r.event)
-    resource := r.resource
-    pathParams, ok := r.event["pathParameters"]
+    var handler func(i *Input) *Response
+    var found bool
 
-    if ok && pathParams != nil {
-        for k, v := range pathParams.(map[string]interface{}) {
-            resource = strings.Replace(resource, v.(string), fmt.Sprintf("{%s}", k), 1)
+    if !r.isSnsEvent() {
+        resource := r.resource
+        pathParams, ok := r.event["pathParameters"]
+        if ok && pathParams != nil {
+            for k, v := range pathParams.(map[string]interface{}) {
+                resource = strings.Replace(resource, v.(string), fmt.Sprintf("{%s}", k), 1)
+            }
         }
+        handler, found = r.routes[resource][r.method]
+    } else {
+        record := r.event["Records"].([]map[string]interface{})[0]
+        message := record["SNS"].(map[string]string)["Message"]
+        // TODO: Implement for SNS
     }
 
-    handler, ok := r.routes[resource][r.method]
-
     var response Response
-    if !ok {
+    if !found {
         return &response, errors.New("handler func missing")
     }
 
     response = *handler(&Input{event: r.event})
     return &response, nil
+}
+
+func (r Request) isSnsEvent() bool {
+    return len(r.event["Records"].([]map[string]interface{})) > 0
 }
