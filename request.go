11fe601b95e00d66165aeddba67e76495b5b250f
Matthis Stenius <matthis@auddly.com>
2018-01-23 12:22:53 +0100

    Can now handle native Lambda go event

--- request.go
+++ request.go
@@ -7,11 +7,9 @@ import (
     "log"
 )
 
-type Request struct {
-    resource      string
-    method        string
-    event         map[string]interface{}
-    handlerConfig *HandlerConfig
+type Handler struct {
+    config *HandlerConfig
+    event  map[string]interface{}
 }
 
 type HandlerConfig struct {
@@ -19,52 +17,38 @@ type HandlerConfig struct {
     Scheduled map[string]func() *Response
 }
 
-func NewRequest(event interface{}, config *HandlerConfig) *Request {
-    resource, httpMethod := "", ""
-    if val, ok := event.(map[string]interface{})["resource"]; ok {
-        resource = val.(string)
-    }
-
-    if val, ok := event.(map[string]interface{})["httpMethod"]; ok {
-        httpMethod = val.(string)
-    }
-
-    return &Request{
-        resource:      resource,
-        method:        httpMethod,
-        event:         event.(map[string]interface{}),
-        handlerConfig: config,
-    }
+func Newhandler(config *HandlerConfig) *Handler {
+    return &Handler{config: config}
 }
 
-// Invoke invoke and handle request by event type.
-// Supported events are: Api and Schedule
-func (r *Request) Invoke() (*Response, error) {
-    log.Printf("Request event: %s", r.event)
+func (h *Handler) Invoke(event interface{}) (*Response, error) {
+    h.event = event.(map[string]interface{})
+    log.Printf("Request event: %s", event)
 
     switch true {
-    case r.isHttpEvent():
-        return r.handleHttpEvent()
-    case r.isScheduledEvent():
-        return r.handleScheduledEvent()
+    case h.isHttpEvent():
+        return h.handleHttpEvent()
+    case h.isScheduledEvent():
+        return h.handleScheduledEvent()
     }
     return nil, errors.New("unknown event")
 }
 
-func (r Request) isHttpEvent() bool {
-    if _, ok := r.event["path"]; ok {
+func (h *Handler) isHttpEvent() bool {
+    if _, ok := h.event["path"]; ok {
         return true
     }
     return false
 }
 
-func (r Request) isScheduledEvent() bool {
-    return r.event["type"] == "schedule"
+func (h *Handler) isScheduledEvent() bool {
+    return h.event["type"] == "schedule"
 }
 
-func (r *Request) handleHttpEvent() (*Response, error) {
-    pathParams, ok := r.event["pathParameters"]
-    resource := r.resource
+func (h *Handler) handleHttpEvent() (*Response, error) {
+    pathParams, ok := h.event["pathParameters"]
+    resource := h.event["resource"].(string)
+    method := h.event["httpMethod"].(string)
 
     if ok && pathParams != nil {
         for k, v := range pathParams.(map[string]interface{}) {
@@ -72,16 +56,17 @@ func (r *Request) handleHttpEvent() (*Response, error) {
         }
     }
 
-    handler, found := r.handlerConfig.Http[resource][r.method]
+    handler, found := h.config.Http[resource][method]
     if !found {
         return nil, errors.New("handler func missing")
     }
 
-    return &*handler(&Input{event: r.event}), nil
+    return &*handler(&Input{event: h.event}), nil
 }
 
-func (r *Request) handleScheduledEvent() (*Response, error) {
-    handler, found := r.handlerConfig.Scheduled[r.resource]
+func (h *Handler) handleScheduledEvent() (*Response, error) {
+    resource := h.event["resource"].(string)
+    handler, found := h.config.Scheduled[resource]
     if !found {
         return nil, errors.New("handler func missing")
     }
