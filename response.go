56d2d6bc707f88ca43d43a1790ed30c65eb4f398
Matthis Stenius <matthis@auddly.com>
2018-04-13 09:38:48 +0200

    Fixed resposne logging

--- response.go
+++ response.go
@@ -1,51 +1,50 @@
 package api
 
 import (
-    "encoding/json"
-    "bitbucket.org/mstenius/logger"
+	"encoding/json"
+
+	"bitbucket.org/mstenius/logger"
 )
 
 type Response struct {
-    StatusCode      int               `json:"statusCode"`
-    Body            interface{}       `json:"body"`
-    Headers         map[string]string `json:"headers"`
-    IsBase64Encoded bool              `json:"isBase64Encoded"`
+	StatusCode      int               `json:"statusCode"`
+	Body            interface{}       `json:"body"`
+	Headers         map[string]string `json:"headers"`
+	IsBase64Encoded bool              `json:"isBase64Encoded"`
 }
 
 func NewResponse(status int, body interface{}) *Response {
-    encoded, _ := json.Marshal(body)
-    logger.WithFields(logger.Fields{
-        "body": encoded,
-    }).Info("response")
+	encoded, _ := json.Marshal(body)
+	logger.WithFields(logger.Fields{"body": string(encoded)}).Info("response")
 
-    return &Response{
-        StatusCode: status,
-        Body:       string(encoded),
-        Headers: map[string]string{
-            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key",
-            "Access-Control-Allow-Methods": "*",
-            "Access-Control-Allow-Origin":  "*",
-        },
-        IsBase64Encoded: false,
-    }
+	return &Response{
+		StatusCode: status,
+		Body:       string(encoded),
+		Headers: map[string]string{
+			"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key",
+			"Access-Control-Allow-Methods": "*",
+			"Access-Control-Allow-Origin":  "*",
+		},
+		IsBase64Encoded: false,
+	}
 }
 
 func NewErrorResponse(status int, error interface{}) *Response {
-    encoded, _ := json.Marshal(map[string]interface{}{
-        "error": error,
-    })
-    logger.WithFields(logger.Fields{
-        "error": encoded,
-    }).Info("Error response")
+	encoded, _ := json.Marshal(map[string]interface{}{
+		"error": error,
+	})
+	logger.WithFields(logger.Fields{
+		"error": string(encoded),
+	}).Info("Error response")
 
-    return &Response{
-        StatusCode: status,
-        Body:       string(encoded),
-        Headers: map[string]string{
-            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key",
-            "Access-Control-Allow-Methods": "*",
-            "Access-Control-Allow-Origin":  "*",
-        },
-        IsBase64Encoded: false,
-    }
+	return &Response{
+		StatusCode: status,
+		Body:       string(encoded),
+		Headers: map[string]string{
+			"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key",
+			"Access-Control-Allow-Methods": "*",
+			"Access-Control-Allow-Origin":  "*",
+		},
+		IsBase64Encoded: false,
+	}
 }
